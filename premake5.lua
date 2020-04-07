da4db4b4ec94757a587690fb63a60d73ea4e8822
mateimicro123 <34721984+mateimicro123@users.noreply.github.com>
2020-04-07 13:41:34 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+ project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "imgui.h",
+        "imgui.cpp",
+        "imconfig.h",
+        "imgui_widgets.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+
+    filter "system:linux"
+        pic "On"
+        staticruntime "On"
+        
+    filter "configurations:Debug"
+        symbols "On"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        optimize "On"
+        runtime "Release"
